185487adcfd9bc8c4f7e503a1be592cc5de9f8f0
myworldzycpc <myworldzhangyichen@qq.com>
2026-06-26 20:57:20 +0800

    init

---
+++ src/client/java/io/github/myworldzycpc/material_preparer/client/MaterialPreparerClient.java
@@ -0,0 +1,10 @@
+package io.github.myworldzycpc.material_preparer.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MaterialPreparerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
